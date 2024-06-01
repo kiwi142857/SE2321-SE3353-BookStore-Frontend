05f8afcb4e4f9067fae3d80024baa767a4a4f4af
kiwi142857 <779490624@qq.com>
2024-06-01 21:19:08 +0800

    增加订单管理

---
+++ src/components/OrderManaProtable.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useRef } from 'react';
+import ProTable from '@ant-design/pro-table';
+import { DatePicker } from 'antd';
+import { searchOrdersAdmin } from '../service/order';
+import moment from 'moment';
+
+const { RangePicker } = DatePicker;
+
+export function OrderManaProtable() {
+
+    const columns = [
+        {
+            title: '订单号', dataIndex: 'id', key: 'id',
+            sorter: (a, b) => a.id - b.id,
+            hideInSearch: true,
+        },
+        {
+            title: '用户ID', dataIndex: 'userId', key: 'userId',
+            sorter: (a, b) => a.userId - b.userId,
+            hideInSearch: true,
+        },
+        {
+            title: '收货人',
+            dataIndex: 'receiver',
+            key: 'receiver',
+            sorter: (a, b) => a.receiver.localeCompare(b.receiver),
+            hideInSearch: true,
+        },
+        {
+            title: '书本名称',
+            key: 'bookTitle',
+            render: (text, record) => (
+                <div>
+                    {record.items.map(item => <div key={item.id}>{item.book.title}</div>)}
+                </div>
+            ),
+        },
+        { title: '联系方式', dataIndex: 'tel', key: 'tel', hideInSearch: true, },
+        { title: '收货地址', dataIndex: 'address', key: 'address', hideInSearch: true, },
+        {
+            title: '下单时间',
+            dataIndex: 'createdAt',
+            key: 'createdAt',
+            valueType: 'dateRange',
+            render: (_, record) => moment(record.createdAt).format('YYYY-MM-DD HH:mm:ss'),
+        },
+    ];
+
+    return <ProTable
+        columns={columns}
+        request={async (params) => {
+            const { current, pageSize, createdAt, ...rest } = params;
+            console.log('params', params);
+            
+            let startTime, endTime;
+            if (createdAt && createdAt.length === 2) {
+                startTime = moment(createdAt[0]).startOf('day').toISOString();
+                endTime = moment(createdAt[1]).endOf('day').toISOString();
+            } else {
+                startTime = "2022-01-01T00:00:00Z";
+                endTime = "2028-01-01T00:00:00Z";
+            }
+            console.log('startTime', startTime);
+            console.log('endTime', endTime);
+            const data = await searchOrdersAdmin(rest.bookTitle || "", current, pageSize, startTime, endTime);
+            const orders = data.orders;
+            console.log('data', data);
+            return {
+                data: orders.map(order => ({
+                    ...order,
+                    key: order.id,
+                })),
+            };
+        }}
+        rowKey="id"
+        search={{
+            labelWidth: 'auto',
+        }}
+        pagination={{
+            pageSize: 10,
+        }}
+        dateFormatter="string"
+        headerTitle="订单管理"
+    />;
+
+}
---
+++ src/components/UserManaProtable.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useRef } from 'react';
+import ProTable from '@ant-design/pro-table';
+
+export function UserManaProtable() {
+    return <ProTable
+        // ...
+    />;
+}
