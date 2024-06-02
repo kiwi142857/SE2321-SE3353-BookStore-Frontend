4521ffacc74d415150483a189fe9e847fb275539
kiwi142857 <779490624@qq.com>
2024-06-02 16:38:17 +0800

    删除无关import“

--- src/components/userSalesProtable.jsx
+++ src/components/userSalesProtable.jsx
@@ -1,20 +1,7 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import ProTable from '@ant-design/pro-table';
-import { DatePicker } from 'antd';
-import { searchOrdersAdmin } from '../service/order';
 import moment from 'moment';
 import { getSalesRankList } from '../service/user';
-
-
-import { Button } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
-import { useState } from 'react';
-import { searchBooks, getBookById, postBook, deleteBook, getBooksBySalesRank } from '../service/book';
-import { Upload } from 'antd';
-import { handleBaseApiResponse } from "../utils/message";
-import { get } from '../service/common';
-
-const { RangePicker } = DatePicker;
 
 export function UserSalesProtable() {
 
