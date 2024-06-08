37aa2c43ab64854576ecf0509c7b3f32074966e4
kiwi142857 <779490624@qq.com>
2024-06-08 13:51:22 +0800

    add check in administrator page

--- src/page/administrator.jsx
+++ src/page/administrator.jsx
@@ -2,17 +2,42 @@
 import { Card } from "antd";
 import { PrivateLayout } from "../components/layout";
 import { Tabs } from 'antd';
-import ProTable from '@ant-design/pro-table';
 import { useState } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 import { BookManaProtable } from "../components/bookManaProtable";
 import { UserManaProtable } from "../components/UserManaProtable";
 import { OrderManaProtable } from "../components/OrderManaProtable";
+import { getMe } from "../service/user";
 const { TabPane } = Tabs;
 
 
 export default function AdministratorPage() {
 
     const [activeTab, setActiveTab] = useState('books');
+
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+
+    const checkLogin = async () => {
+        let me = await getMe();
+        
+        if (!me) {
+            navigate("/login");
+        } else {
+            setUser(me);
+        }
+        if(me.role != 1) {
+            navigate("/home");
+        }
+    }
+
+    useEffect(() => {
+        checkLogin();
+        // eslint-disable-next-line
+    }, []);
+
+    
 
     return <PrivateLayout>
         <Card style={{ margin: '10px', marginTop: '20px' }}>
